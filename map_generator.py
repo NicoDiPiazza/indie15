9080555519d0b2c632368ecbad3e990b1aefb5b2
Domenico Di Piazza <nicodipiazza1@gmail.com>
2024-04-27 21:24:06 -0700

    Update map_generator.py

--- map_generator.py
+++ map_generator.py
@@ -1,10 +1,20 @@
 from random import random
+import math
 def newMap():
-    heightBound = 20
+    heightBound = 40
     generatedMap = [[0,0]]
 
-    roadOptions = ['normal', 'upRamp', 'gap']
+    roadOptions = ['start', 'normal', 'ice', 'upRamp', 'gap']
+    normalOptions = ['normal', 'normal', 'normal', 'normal', 'normal', 'normal', 'normal', 'ice', 'normal', 'upRamp']
+    iceOptions = ['normal', 'normal', 'ice', 'ice', 'ice', 'ice', 'ice', 'ice', 'ice', 'ice']
+    # gap has 100% chance to follow ramp
+    #ice has a 60% chance to follow ice
+    #road has a 70% chance to follow road
+    #ramp has 10% to follow anything (other than ramp)
+    #each point has a 10% chance of being a checkpoint
+    #the starting point is always a road
 
+#first three quarters
     for j in range(4* heightBound):
         if j < heightBound:
             newPoint = [generatedMap[j][0] + 1, generatedMap[j][1] - round(random())]
@@ -13,7 +23,9 @@ def newMap():
         elif (j < 3 * heightBound):
             newPoint = [generatedMap[j][0] - 1, generatedMap[j][1] + round(random())]
         generatedMap.append(newPoint)
-    
+
+
+#last quarter    
     while generatedMap[len(generatedMap) -1] != [0,0]:
         if generatedMap[len(generatedMap) -1][0] > 0:
             newPoint = [generatedMap[len(generatedMap) -1][0] -1]
@@ -25,6 +37,32 @@ def newMap():
             newPoint.append(0)
         generatedMap.append(newPoint)
 
+#assigning each node a road type
+    for i in range(len(generatedMap)-1):
+        if(i == 0):
+            generatedMap[i].append('start')
+        elif generatedMap[i-1][2] == 'normal' or generatedMap[i-1][2] == 'gap':
+            assignment = normalOptions[math.floor(random() * 10)]
+            generatedMap[i].append(assignment)
+        elif generatedMap[i-1][2] == 'ice':
+            assignment = iceOptions[math.floor(random() * 10)]
+            generatedMap[i].append(assignment)
+        elif  (generatedMap[i-1][2] == 'upRamp') and i != len(generatedMap)-2:
+            assignment = 'gap'
+            generatedMap[i].append(assignment)
+        else:
+            generatedMap[i].append('normal')
+
+        checkpoint = random()
+        if checkpoint <= 0.07:
+            generatedMap[i].append('checkpoint')
+        else:
+            generatedMap[i].append('not checkpoint')
+
+
+
+
+    return generatedMap
 
 
     return generatedMap
